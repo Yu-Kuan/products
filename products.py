17a4ae3c39886610275a2fe11a567eaf3934ffd9
Ken Huang <hunm4dj0@gmail.com>
2019-08-28 22:43:22 +0800

    add raed and split

--- products.py
+++ products.py
@@ -1,4 +1,10 @@
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		name, price = line.strip().split(',') #split()完會變清單
+		products.append([name, price])
+print(products)
+
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
